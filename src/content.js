7bfaf519dc26f9c3ab098e569fcbe553a2f87aa9
Tomas <46719035+toooch@users.noreply.github.com>
2023-11-16 23:21:58 +0100

    Update content.js
    Removed parts that are not used in this version

--- src/content.js
+++ src/content.js
@@ -55,96 +55,9 @@ var click = new MouseEvent("click", {
  * TODO: Tidy up
  */
 function runContentScript() {
-    
-    // Add the toggle button to the youtube ui
-    //// injectButton(buttonClass);
-
-    // Grab our button for future use
-    //// let btn = document.querySelector("."+buttonClass);
-
-    // Update the button with the saved state once
-    //// updateButtonState(btn);
-
-    // Attach the click listener to the button
-    //// attachListener(btn);
-
     setInterval(blockVideoAds(), 50);
     setInterval(blockStaticAds(), 50);
-
-    // chrome.contextMenus.onClicked.addListener(genericOnClick);
-
 }
-
-/**
- * Inject a new button with given class name into the control bar of a YouTube video
- * @param {string} className 
- */
-function injectButton(className) {
-    const html = `
-    <button class="${className} ytp-button" abbb-pressed="0" aria-keyshortcuts="b" data-priority="3" data-title-no-tooltip="ABBB" aria-pressed="false" aria-label="ABBB shortcut b" style title="ABBB (b)">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <g>
-            <path fill="none" d="M0 0h24v24H0z"/>
-            <path fill-rule="nonzero" d="M21 3a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18zm-1 2H4v14h16V5zM9.399 8l3.199 8h-2.155l-.4-1h-3.29l-.4 1H4.199l3.2-8h2zM19 8v8h-3a3 3 0 0 1 0-6h.999L17 8h2zm-2 4h-1a1 1 0 0 0-.117 1.993L16 14h1v-2zm-8.601-1.115L7.552 13h1.692l-.845-2.115z"/>
-        </g>
-    </svg>
-    </button>
-    `;
-
-    let rightControls = document.querySelector(".ytp-right-controls");
-    rightControls?.insertAdjacentHTML("afterbegin", html);
-
-    const css = `
-    <style>
-    .${className}[abbb-pressed="0"] svg { filter: invert(100%) sepia(91%) saturate(139%) hue-rotate(186deg) brightness(108%) contrast(87%); margin: 15%; transition: all 0.5s; }
-    .${className}[abbb-pressed="1"] svg { filter: invert(15%) sepia(100%) saturate(6890%) hue-rotate(355deg) brightness(101%) contrast(117%); margin: 15%; transition: all 0.5s; }
-    .${className} {  }
-    </style>
-    `;
-
-    let head = document.head || document.documentElement;
-    head.insertAdjacentHTML("beforeend", css);
-}
-
-/**
- * Update the state of the given element with the stored data
- * @param {string} el 
- */
-function updateButtonState(el) {
-    chrome.storage.local.get({abbb_enable: 1}).then((result) => {
-        el?.setAttribute("abbb-pressed", result.abbb_enable);
-    });
-}
-
-/**
- * Attach an event listener to the given element
- * @param {string} el 
- */
-function attachListener(el) {
-    el?.addEventListener('click', () => {
-        toggleState(el);
-    });
-}
-
-/**
- * Toggle the abbb-state of the given element
- * @param {string} el 
- */
-function toggleState(el) {
-    // if (el != undefined) {
-        let btn = el;
-        let state = btn.getAttribute("abbb-pressed");
-
-        if (state == 0) { // turn on
-            btn.setAttribute("abbb-pressed", 1);
-            chrome.storage.local.set({ abbb_enable: 1 });
-        } else { // turn off
-            btn.setAttribute("abbb-pressed", 0);
-            chrome.storage.local.set({ abbb_enable: 0 });
-        }
-    // }
-}
-
 
 /**
  * Blocks static ads on a YouTube webpage
@@ -240,18 +153,6 @@ function blockVideoAds(knownVariations) {
 
     }
 }
-
-// function genericOnClick(info) {
-//     switch (info.menuItemId) {
-//         case 'debugEnable':
-//             debug = true;
-//             break;
-
-//         case 'debugDisable':
-//             debug = false;
-//             break;
-//     }
-// }
 
 /**
  * Logs the given message when the debug mode is turned on
